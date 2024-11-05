c303c71c2f08a983e3bbb7ea98117aaed93c5963
Ryan DECIAN <ryan.decian.pro@gmail.com>
2024-11-05 16:08:48 +0100

    Création de la branch props.2

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ import { useState } from "react";
 const pokemonList = [
   {
     name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
     name: "mew",
