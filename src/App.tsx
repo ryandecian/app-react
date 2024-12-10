c3fe51f26fc3319f6fca2f0ea7c4bdab51dc2426
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-10 19:37:46 +0100

    Solution react basics 7

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,46 @@ const pokemonList = [
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        id: 0,
     },
     {
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        id: 1,
     },
     {
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+        id: 2,
     },
     {
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+        id: 3,
     },
     {
       name: "mew",
+      id: 4,
     },
   ];
 
-function App() {
-
-  const [pokemonIndex, setPokemonIndex] = useState(0)
-  const incrementation = () => {
-    setPokemonIndex(pokemonIndex + 1)
+  function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+  
+    return (
+      <div>
+        <nav>
+           {pokemonList.map((Var) => {
+            return (
+              <button key={Var.id} onClick = {() => setPokemonIndex(Var.id)}>{Var.name}</button>
+           )})}
+        </nav>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      </div>
+    );
   }
-  const decrementation = () => {
-    setPokemonIndex(pokemonIndex - 1)
-  }
-
-  return (
-    <div>
-       <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
-       {pokemonIndex > 0 && <button onClick = {decrementation}>Précédent</button>}
-       {pokemonIndex < 4 && <button onClick = {incrementation}>Suivant</button>}
-    </div>
-  );
-}
 
 export default App;
